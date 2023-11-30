fdffea8a9eac82aeca6d6e81c0079f7e76055ce8
Lindsay Jadow <lindsayjadow@lindsays-mbp.mynetworksettings.com>
2023-11-30 15:39:54 -0500

    changed foreach to a map

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //state
-let state = [];
+let state = {
+    events: []
+};
 
 //container elements 
 const partiesContainer = document.getElementById('partyList')
@@ -8,38 +10,45 @@ const partiesContainer = document.getElementById('partyList')
 const APIurl = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF'
 
 //fetch recipes
-async function fetchAPIevents () {
-    try{
-        const APIevents = await fetch(`${APIurl}/events`);
-        const APIconverted = await APIevents.json();
-        console.log(APIconverted);
-        state = APIconverted.data
-        return state;
-    } catch (error){
+async function fetchAPIevents() {
+    try {
+        const APIresponse = await fetch(`${APIurl}/events`);
+        const APIconverted = await APIresponse.json()
+        state.events = APIconverted.data
+        return state.events;
+    } catch (error) {
         console.log(error);
     }
 }
 
 //render function 
-function renderEvents(eventsParam){ //events is the param -- events from fetchAPIevents will feed into it
-    eventsParam.forEach((event,index) => {
-        const event_name = document.createElement('div')
-        event_name.className=`eventClass`
-        event_name.innerHTML=`
-        <h2>Event: ${event.name}</h2>
-        <h3>When: ${event.date}</h3>
-        <h3>Where: ${event.location}</h3>
-        <p>Description: <br/>${event.description}</p>
-        <button id='deleteButton'>Delete</button>`
-        partiesContainer.append(event_name)
-        console.log(event_name)
+function renderEvents(eventsParam) { // const events from fetchAPIevents() will feed into (eventsParam)
+    
+    //if statement for if no data is found
+    if(!eventsParam || eventsParam.length === 0){
+        eventsParam.innerHTML = '<h3>No data found</h3>';
+        return;
+    }
+    
+    const eventsData = state.events.map((ele, ind) => {
+        const event = document.createElement('div')
+        event.className = `event`
+        //event.setAttribute('id',`num${ind}`)
+        event.innerHTML = `
+            <h2>Event: ${ele.name}</h2>
+            <h3>When: ${ele.date}</h3>
+            <h3>Where: ${ele.location}</h3>
+            <p>Description: <br/>${ele.description}</p>
+            <button class='deleteButton'>Delete</button>`;
+        return event;
+        
     })
+    partiesContainer.replaceChildren(...eventsData)
 }
 
 //init for the pageload
-async function initialize(){
+async function initialize() {
     const events = await fetchAPIevents();
-
     renderEvents(events); //taking events from line --> result of the fetch
 }
 
